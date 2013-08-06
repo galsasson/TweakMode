788c0fdcec08ccde4bc1274389e42113b6f844d1
Gal Sasson <sasgal@gmail.com>
2013-08-05 23:31:14 -0400

    changed mouse cursor when over a number

--- src/processing/mode/tweak/TweakTextAreaPainter.java
+++ src/processing/mode/tweak/TweakTextAreaPainter.java
@@ -237,13 +237,13 @@ public class TweakTextAreaPainter extends TextAreaPainter
 			
 			if (n.pick(mouseX, mouseY))
 			{
-				cursorType = Cursor.HAND_CURSOR;
+				cursorType = Cursor.W_RESIZE_CURSOR;
 				setCursor(new Cursor(cursorType));
 				return;
 			}
 		}
 		
-		if (cursorType == Cursor.HAND_CURSOR || cursorType == -1) {
+		if (cursorType == Cursor.W_RESIZE_CURSOR || cursorType == -1) {
 			cursorType = Cursor.DEFAULT_CURSOR;
 			setCursor(new Cursor(cursorType));
 		}		
